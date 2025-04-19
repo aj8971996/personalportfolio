43825d48c6dd0604d2a403709f06b32ad1ca1cee
aj8971996 <70617301+aj8971996@users.noreply.github.com>
2025-04-18 18:48:55 -0700

    Working on homepage and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,19 +8,9 @@ export const routes: Routes = [
     title: 'Home'
   },
   {
-    path: 'projects',
-    loadComponent: () => import('./features/projects/projects-list/projects-list.component').then(m => m.ProjectsListComponent),
-    title: 'Projects'
-  },
-  {
     path: 'resume',
     loadComponent: () => import('./features/resume/resume.component').then(m => m.ResumeComponent),
     title: 'Resume'
-  },
-  {
-    path: 'skills',
-    loadComponent: () => import('./features/skills/skills.component').then(m => m.SkillsComponent),
-    title: 'Skills'
   },
   {
     path: '**',
